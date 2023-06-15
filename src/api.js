252c8a1e339893e6edebc719393285ff50e88bb6
Wei Wang <weiwng@google.com>
2023-06-15 16:32:37 -0400

    test

--- src/api.js
+++ src/api.js
@@ -1,14 +1,20 @@
-import * as AWS from "aws-sdk";
+import { Client, Storage, InputFile } from "node-appwrite";
 
 export async function initApi() {
-  // TODO
-  return null;
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1") // Your Appwrite Endpoint
+    .setProject("6487629c7b2b884402f4") // Your project ID 
+  return new Storage(client);
 }
 
 export async function uploadFile(api, fileName, file) {
-  // TODO
+  await api.createFile(
+    "6488c941246c76aa73d6",
+    fileName,
+    InputFile.fromBuffer(file, fileName)
+  );
 }
 
 export async function downloadFile(api, fileName) {
-  // TODO
+  const response = await api.getFileView("6488c941246c76aa73d6", fileName);
 }
